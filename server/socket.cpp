bb1889566cf8f4d90567f35a7437dd539162b461
Adam <adambachmann7@gmail.com>
2025-02-03 22:15:29 +0100

    added error codes for socket

--- server/socket.cpp
+++ server/socket.cpp
@@ -5,10 +5,10 @@ int Socket::create_socket()
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
-        status = -1;
+        status = CREATE_ERROR;
     }
     else{
-        status = 0;
+        status = NO_ERROR;
     }
 
     return status;
@@ -19,13 +19,13 @@ int Socket::socket_options()
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
-        status = -1;
+        status = OPTIONS_ERROR;
     }
     else {
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY;
         address.sin_port = htons(port);
-        status = 0;
+        status = NO_ERROR;
     }
 
     return status;
@@ -36,10 +36,10 @@ int Socket::bind_socket()
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
-        status = -1;
+        status = BIND_ERROR;
     }
     else{
-        status = 0;
+        status = NO_ERROR;
     }
 
     return status;
@@ -50,10 +50,10 @@ int Socket::listen_socket()
     if (listen(server_fd, 3) < 0) {
         perror("Listen");
         exit(EXIT_FAILURE);
-        status = -1;
+        status = LISTEN_ERROR;
     }
     else{
-        status = 0;
+        status = NO_ERROR;
     }
 
     return status;
@@ -64,10 +64,10 @@ int Socket::accept_socket()
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("Accept");
         exit(EXIT_FAILURE);
-        status = -1;
+        status = ACCEPT_ERROR;
     }
     else{
-        status = 0;
+        status = NO_ERROR;
     }
 
     return status;
--- server/socket.hpp
+++ server/socket.hpp
@@ -4,6 +4,16 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+enum ERROR{
+    NO_ERROR,
+    INIT_ERROR,
+    CREATE_ERROR,
+    OPTIONS_ERROR,
+    BIND_ERROR,
+    LISTEN_ERROR,
+    ACCEPT_ERROR
+};
+
 class Socket {
 public:
     // methods for socket
@@ -17,6 +27,7 @@ public:
     // getters and setters
     int get_new_socket() { return new_socket; };
     int get_server_fd() { return server_fd; };
+    int get_status() { return status; };
 
 private:
     int status;
